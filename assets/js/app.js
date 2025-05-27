4fb3efd2c33b05af51ccba364ab562dd5e5eb62e
Abigail Galvez <galvezabigail79@gamil.com>
2025-05-26 22:07:23 -0600

    Primer commit: agregando mi carpeta

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,23 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const $n = document.querySelector('.name'); //.name para seleccionar correctamente la clase
+const $b = document.querySelector('.blog'); //.blog para seleccionar correctamente la clase
+const $l = document.querySelector('.location'); //integrarlo en el html
 
-function displayUser(username) {
+async function displayUser(username) { //await debe usarse dentro de una función async
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
+  const data = await response.json(); //convertir json
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  $n.textContent = `${data.name}`; //comillas inversas
+  $b.textContent = `${data.blog}`; //comillas inversas
+  $l.textContent = `${data.location}`; //comillas inversas
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}` //usar $n 
 }
 
 displayUser('stolinski').catch(handleError);
